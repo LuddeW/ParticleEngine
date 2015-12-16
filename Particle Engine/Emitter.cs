572cbfd9548373ee1a5134ab33690b8b755b2e2f
LuddeW <ludwig.wiktorsson@gmail.com>
2015-12-16 22:03:44 +0100

    Working, need adjustments.

---
+++ Particle Engine/Emitter.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Particle_Engine
+{
+    public class Emitter
+    {
+        private Random random;
+        public Vector2 EmitterLocation { get; set; }
+        private List<Particle> particles;
+        private List<Texture2D> textures;
+
+        public Emitter(List<Texture2D> textures, Vector2 location)
+        {
+            EmitterLocation = location;
+            this.textures = textures;
+            this.particles = new List<Particle>();
+            random = new Random();
+        }
+
+        private Particle GenerateNewParticle()
+        {
+            Texture2D texture = textures[random.Next(textures.Count)];
+            Vector2 position = EmitterLocation;
+            Vector2 velocity = new Vector2(
+                    1f * (float)(random.NextDouble() * 2 - 1),
+                    1f * (float)(random.NextDouble() * 2 - 1));
+            float angle = 0;
+            float angularVelocity = 0.1f * (float)(random.NextDouble() * 2 - 1);
+            int red = (byte) random.Next(40, 80);
+            int green = (byte)random.Next(170, 230);
+            int blue = (byte)random.Next(210, 255);
+            Color color = new Color(red, green, blue);
+                   
+            float size = (float)random.NextDouble();
+            int ttl = 20 + random.Next(40);
+
+            float layerDepth = (float)random.NextDouble();
+            return new Particle(texture, position, velocity, angle, angularVelocity, color, size, ttl, layerDepth);
+        }
+
+        public void Update()
+        {
+            int total = 10;
+
+            for (int i = 0; i < total; i++)
+            {
+                particles.Add(GenerateNewParticle());
+            }
+
+            for (int particle = 0; particle < particles.Count; particle++)
+            {
+                particles[particle].Update();
+                if (particles[particle].TTL <= 0)
+                {
+                    particles.RemoveAt(particle);
+                    particle--;
+                }
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            for (int i = 0; i < particles.Count; i++)
+            {
+                particles[i].Draw(spriteBatch);
+            }
+            spriteBatch.End();
+        }
+
+    }
+}
